43e7fe283034557e3b61cd3dc305c89659a1c191
tasim313 <mostasimmahmudtasim@gmail.com>
2023-07-18 12:04:13 +0600

    add permission class and comment

--- core/rest/views/document.py
+++ core/rest/views/document.py
@@ -5,10 +5,12 @@ from rest_framework import filters
 
 from ...models import Document
 from ..serializers import document
+from ..permission import base
 
 
 
 class DocumentList(generics.ListAPIView):
+    """authorize and unauthorize user can see document meta data"""
     
     queryset = Document.objects.filter(status="Active")
     serializer_class = document.DocumentSerializer
@@ -20,23 +22,23 @@ class DocumentList(generics.ListAPIView):
                       'created_date',
                       'updated_date',
                     ]
-
+    
 
 
 class DocumentCreate(generics.ListCreateAPIView):
+    
+    """authorize  user can create and see his document meta data. also search document by title,description,file,created_date, """
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, base.IsOwnerOrAdminOrReadOnly]
     queryset = Document.objects.filter(
         status="Active")
     serializer_class =  document.DocumentSerializer
 
-    filter_backends = (filters.SearchFilter,)
-    search_fields =  ['title',
-                      'description',
-                      'file',
-                      'created_date',
-                      'updated_date',
-        ]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Document.objects.filter(user_created=user)
+    
 
     def post(self, request, format=None, **kwargs):
         serializer = document.DocumentCreateSerializer(
@@ -46,16 +48,21 @@ class DocumentCreate(generics.ListCreateAPIView):
         serializer.save()
         return Response(serializer.data)
     
+    
 
 class DocumentRetrieveUpdate(generics.RetrieveUpdateAPIView):
-    permission_classes = [IsAuthenticated]
+     
+    """authorize  user can update their document meta data. by uid """
+
+    permission_classes = [IsAuthenticated, base.IsOwnerOrAdminOrReadOnly]
     queryset = Document.objects.all()
     serializer_class = document.DocumentUpdateSerializer
     lookup_field = 'uid'
 
 
 class DocumentShareOrDownloadList(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
+    """authorize  user can share and download document meta data. by url path """
+    permission_classes = [IsAuthenticated, base.IsOwnerOrAdminOrReadOnly]
     queryset = Document.objects.filter(status="Active")
     serializer_class = document.FileShareOrDownloadSerializer
 
@@ -66,3 +73,7 @@ class DocumentShareOrDownloadList(generics.ListAPIView):
                       'created_date',
                       'updated_date',
                     ]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Document.objects.filter(user_created=user)
